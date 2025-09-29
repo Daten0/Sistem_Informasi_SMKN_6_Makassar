561ad364075ef3729d1378a524e26994d7a17afb
Daten <febrimicrone@gmail.com>
2025-09-29 10:59:34 +0800

    Penambahan Field Mapel, dan Kelas di profil

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -37,7 +37,7 @@ const Sidebar = ({ isOpen }: SidebarProps) => {
           <li>
             <Link to="/admin/berita" className="flex items-center px-6 py-3 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700">
               <NewspaperIcon className="w-5 h-5" />
-              <span className="ml-4">Berita Sekolah</span>
+              <span className="ml-4">Berita Sekolah</span> 
             </Link>
           </li>
         </ul>
